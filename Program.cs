91e7c0992521266023f2b04241e3dc1470b2d3f7
abhinav9691 <81373991+abhinav9691@users.noreply.github.com>
2023-06-19 01:13:33 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using MoodAnalyser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+
+namespace MoodAnalyserTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        //Test Case 1.1  Given I am in Sad Mood Message Should return Sad Mood
+
+        [TestMethod]
+        public void GivenSADMood_ShouldReturnSAD()
+        {
+
+            //Arrange
+
+            string message = "I am in Sad Mood";
+            MoodAnalyse obj = new MoodAnalyse(message);
+
+            string expected = "Sad";
+
+            //Act
+
+            string actual = obj.AnalyseMood();
+
+            //Assert
+
+            Assert.AreEqual(expected, actual);
+        }
+        //Test Case 1.2  Given I am in Happy Mood Message Should return Happy Mood
+
+        [TestMethod]
+        public void GivenHappyMoodMessage_ShouldReturnHAPPY()
+        {
+
+            //Arrange
+
+            string message = "I am in Happy Mood";
+            MoodAnalyse obj = new MoodAnalyse(message);
+
+            string expected = "Happy";
+
+            //Act
+
+            string actual = obj.AnalyseMood();
+
+            //Assert
+
+            Assert.AreEqual(expected, actual);
+        }
+        //Test Case 2.1 Given Null Mood Should Return Happy
+
+
+        [TestMethod]
+        public void GivenNullMood_ShouldReturnHappy()
+        {
+            //Arrange
+            string message = null;
+            MoodAnalyse obj = new MoodAnalyse(message);
+            //Act
+            string actual = obj.AnalyseMood();
+            //Assert
+            Assert.AreEqual(actual, "Happy");
+        }
+
+    }
+}
